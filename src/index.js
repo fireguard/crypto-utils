403c761cd2e3c5308308d1d9d0efe7a1a0785416
Ronaldo Meneguite <ronaldo@fireguard.com.br>
2020-08-19 15:34:26 -0300

    Implement otp utils

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@
 const { Command } = require('commander');
 const { genPassword, genSalt, genHash } = require('./password');
 const { genToken, isValidToken } = require('./token');
+const { checkTOtpCode, genTOtpCode, checkAuthenticatorCode, genAuthenticatorCode, genAuthenticatorSecret } = require('./otp');
 
 const program = new Command();
 program.version('1.0.1', '-v, --version', 'output the current version');
 
 const defaultPasswordLength = 16;
+const defaultSecretOtpBytes = 16;
 
 const parseInteger = (value, dummyPrevious) => {
   return parseInt(value, 10);
@@ -52,6 +54,50 @@ program.command('token [args]')
     const algorithm = cmd.algorithm || 'sha256';
     const token = genToken(args, algorithm);
     console.info(`Token: ${token}`);
+  });
+
+program.command('authenticator [secret]')
+  .option('-b, --bytes <bytes>', `Set secret number of bytes (Default: ${defaultSecretOtpBytes})`, parseInteger, defaultSecretOtpBytes)
+  .option('-c, --check <code>', 'Check is valid Code')
+  .option('-d, --digits <digits>', 'Number of digits for generate authenticator code (Default: 6)', parseInteger, 6)
+  .action((secret, cmd) => {
+    if (!secret) {
+      const bytes = cmd.bytes || defaultSecretOtpBytes;
+      const secret = genAuthenticatorSecret(bytes);
+      console.info(`Secret: ${secret}`);
+      return;
+    }
+    const digits = cmd.digits || 6;
+    const options = { digits };
+
+    if (cmd.check) {
+      const result = checkAuthenticatorCode(secret, cmd.check, options);
+      console.info(`Result: ${ result ? 'VALID' : 'INVALID' }`);
+      return;
+    }
+
+    const code = genAuthenticatorCode(secret, options);
+    console.info(`Code: ${code}`);
+  });
+
+program.command('totp <secret>')
+  .option('-c, --check <code>', 'Check is valid Code')
+  .option('-d, --digits <digits>', 'Number of digits for generate totp code (Default: 6)', parseInteger, 6)
+  .action((secret, cmd) => {
+    if (!secret) {
+      throw new Error('Invalid required param');
+    }
+    const digits = cmd.digits || 6;
+    const options = { digits };
+
+    if (cmd.check) {
+      const result = checkTOtpCode(secret, cmd.check, options);
+      console.info(`Result: ${ result ? 'VALID' : 'INVALID' }`);
+      return;
+    }
+
+    const code = genTOtpCode(secret, options);
+    console.info(`Code: ${code}`);
   });
 
 
